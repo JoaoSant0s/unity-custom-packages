264ad74fa5244a3d0d0f8e6755bfe65eb900896e
João Ricardo dos Santos <Ricardoelric@gmail.com>
2021-11-18 00:07:30 -0300

    imp: making first implementating and tests with new PlayerPrefs alternatives

--- Assets/_Project/Scripts/TestSaveService.cs
+++ Assets/_Project/Scripts/TestSaveService.cs
@@ -14,9 +14,32 @@ public class TestSaveService : MonoBehaviour
         saveService = Services.Get<SaveService>();
     }
 
-
     private void Start() 
     {
-        Debug.Log(saveService.Contains("test"));
+        var testIntUnit = "testIntUnit";
+        var testFloatUnit = "testFloatUnit";
+        var testStringUnit = "testStringUnit";
+        var testBoolUnit = "testBoolUnit";
+        var testVector3Unit = "testVector3Unit";
+
+        saveService.Set<int>(testIntUnit, -11);
+        Debug.Log(saveService.Get<int>(testIntUnit));
+
+        saveService.Set<float>(testFloatUnit, -11.2031265f);
+        Debug.Log(saveService.Get<float>(testFloatUnit));
+
+        saveService.Set<string>(testStringUnit, "testStringUnit");
+        Debug.Log(saveService.Get<string>(testStringUnit));
+
+        saveService.Set<bool>(testBoolUnit, true);
+        Debug.Log(saveService.Get<bool>(testBoolUnit));
+
+        Debug.Log(saveService.GetOrDefault<Vector3>("noValue", Vector3.zero));
+        
+        saveService.Set<Vector3>(testVector3Unit, new Vector3(1.0001f, -11.0001f, -11.2031201f));
+        var value = saveService.Get<Vector3>(testVector3Unit);
+        Debug.Log(value);
+        transform.Translate(value);
     }
 }
+
--- Packages/com.joaosantos.package-services/Scripts/Save/SaveService.cs
+++ Packages/com.joaosantos.package-services/Scripts/Save/SaveService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 
 using UnityEngine;
 
+using JoaoSant0s.CommonWrapper;
 using JoaoSant0s.ServicePackage.General;
 
 namespace JoaoSant0s.ServicePackage.Save
@@ -24,19 +25,46 @@ namespace JoaoSant0s.ServicePackage.Save
 
         
         #region Public Methods
+
         public bool Contains(string radicalKey)
         {
-            return PlayerPrefs.HasKey(BuildKey(radicalKey));
+            var key = BuildKey(radicalKey);
+
+            return PlayerPrefs.HasKey(key);
         }
 
-        public void Save<T>(string radicalKey, T value) where T : UnityEngine.Object
+        public bool DeleteSave(string radicalKey)
         {
+            var key = BuildKey(radicalKey);
 
+            if(!Contains(key)) return false;
+
+            PlayerPrefs.DeleteKey(key);
+
+            return true;
         }
 
-        public T Get<T> (string radicalKey) where T : UnityEngine.Object
+        public T GetOrDefault<T>(string radicalKey, T defaultValue = default(T))
         {
-            return null;
+            var key = BuildKey(radicalKey);
+
+            if (Contains(key))
+                return Get<T>(key);
+            else
+                return defaultValue;
+        }      
+
+        public T Get<T>(string radicalKey)
+        {
+            var key = BuildKey(radicalKey);
+            
+            return GetUnit<T>(key);
+        }
+
+        public void Set<T>(string radicalKey, T value)
+        {
+            var key = BuildKey(radicalKey);
+            SetUnit<T>(key, value);
         }
 
         #endregion
@@ -48,7 +76,86 @@ namespace JoaoSant0s.ServicePackage.Save
             return string.Format("{0}_{1}", config.Prefix, radical);
         }
 
+        private T GetUnit<T>(string key)
+        {
+            var type = typeof(T);
+
+            if (type == typeof(int))
+            {
+                return (T) Convert.ChangeType(PlayerPrefs.GetInt(key), type);  
+
+            }else if (type == typeof(float))
+            {
+                return (T) Convert.ChangeType(PlayerPrefs.GetFloat(key), type);                 
+            }
+
+            var stringValue = PlayerPrefs.GetString(key);
+
+            Debugs.Log(stringValue, type, type == typeof(object));
+
+            if (type == typeof(object))
+            {
+                return JsonUtility.FromJson<T>(stringValue);               
+            }else
+            {
+                if(type == typeof(Vector3))
+                {
+                    var auxValue = JsonUtility.FromJson<Vector3Value>(stringValue);
+                    return (T)Convert.ChangeType(auxValue.value, type);
+                }
+                else
+                {
+                    return (T)Convert.ChangeType(stringValue, type);
+                }
+            }
+        }
+
+        private void SetUnit<T>(string key, T tValue)
+        {
+            var type = typeof(T);
+            Debugs.Log(tValue, type);
+
+            if (type == typeof(int))
+            {
+                PlayerPrefs.SetInt(key, ((int) Convert.ChangeType(tValue, type)));
+
+                return;
+
+            }else if (type == typeof(float))
+            {
+                PlayerPrefs.SetFloat(key, ((float) Convert.ChangeType(tValue, type)));
+
+                return;
+            }
+
+            var value = "";
+
+            if (type == typeof(object))
+            {
+                value = JsonUtility.ToJson(tValue);
+            }else
+            {
+                if(type == typeof(Vector3))
+                {
+                    Vector3 v = ((Vector3) Convert.ChangeType(tValue, type));
+                    value = JsonUtility.ToJson(new Vector3Value(){value = v});
+                }else
+                {
+                    value = tValue.ToString();                    
+                }
+            }
+            
+            PlayerPrefs.SetString(key, value);
+        }
+
         #endregion
         
+    }
+
+
+    [Serializable]
+    internal class Vector3Value
+    {
+        public Vector3 value;
     }
 }
