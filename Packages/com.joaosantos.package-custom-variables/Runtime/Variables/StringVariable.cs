db7098b29dc408a06b1b7ca00511d29c18567146
João Santos <ricardoelric@gmail.com>
2022-07-11 15:15:03 -0300

    imp: creating String Variable

---
+++ Packages/com.joaosantos.package-custom-variables/Runtime/Variables/StringVariable.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace JoaoSant0s.CustomVariable
+{
+    [CreateAssetMenu(fileName = "StringVariable", menuName = "JoaoSant0s/CustomVariablePackage/StringVariable")]
+    public class StringVariable : Variable<string>
+    {
+        #region Protected Override Methods
+
+        protected sealed override void OnModify(string newValue)
+        {
+            Value = newValue;
+        }
+
+        #endregion
+
+        #region Public Override Methods
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}", typeof(StringVariable), Value);
+        }
+
+        #endregion
+    }
+}
