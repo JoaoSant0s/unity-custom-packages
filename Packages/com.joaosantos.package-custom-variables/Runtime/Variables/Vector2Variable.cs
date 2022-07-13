e38807379d441c71be09383f60b6537878a1bccb
João Santos <ricardoelric@gmail.com>
2022-07-12 23:53:34 -0300

    wip: making the Vector 2 Variable
    - we must implement some tests for other methods(AddXAxis, AddYAxis, SetXAxis, SetYAxis)

---
+++ Packages/com.joaosantos.package-custom-variables/Runtime/Variables/Vector2Variable.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace JoaoSant0s.CustomVariable
+{
+    [CreateAssetMenu(fileName = "Vector2Variable", menuName = "JoaoSant0s/CustomVariablePackage/Vector2Variable")]
+    public class Vector2Variable : Variable<Vector2>
+    {
+
+        #region Protected Override Methods
+
+        protected override void OnModify(Vector2 newValue)
+        {
+            Value = newValue;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Add(Vector2 increment)
+        {
+            Modify(Value + increment);
+        }
+
+        public void AddXAxis(float xIncrementValue)
+        {
+            Modify(Value + new Vector2(xIncrementValue, 0));
+        }
+
+        public void AddYAxis(float yIncrementValue)
+        {
+            Modify(Value + new Vector2(0, yIncrementValue));
+        }
+
+        public void SetXAxis(float newXValue)
+        {
+            Modify(new Vector2(newXValue, Value.y));
+        }
+
+        public void SetYAxis(float newYValue)
+        {
+            Modify(new Vector2(Value.x, newYValue));
+        }
+
+        #endregion
+    }
+}
