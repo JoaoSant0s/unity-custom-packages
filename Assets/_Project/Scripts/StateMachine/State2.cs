1202432bd10c96bbdd6c2b3c7e7a8850dc9c3e5d
João Santos <ricardoelric@gmail.com>
2022-07-30 16:30:26 -0300

    refactor: simplify state machine scripts and references
    - creating state machine test scene and scripts

---
+++ Assets/_Project/Scripts/StateMachine/State2.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using JoaoSant0s.StateMachine;
+
+public class State2 : State<TesteStateMachine>
+{
+    public State2(TesteStateMachine controller) : base(controller) { }
+
+    #region Public Implemented Methods
+
+    public override void OnBeging()
+    {
+        Debug.Log("OnBeging State2");
+        this.machineController.StartCoroutine(ChangeState());
+    }
+
+    public override void OnFixedUpdate() { }
+    public override void OnLateUpdate() { }
+    public override void OnUpdate() { }
+
+    public override void OnFinish()
+    {
+        Debug.Log("OnFinish State2");
+    }
+
+    #endregion
+
+    #region Private methods
+
+    private IEnumerator ChangeState()
+    {
+        yield return new WaitForSeconds(3f);
+        ChangeState(new State1(this.machineController));
+    }
+
+    #endregion
+}
+
--- Packages/com.joaosantos.package-state-machine/Runtime/State.cs
+++ Packages/com.joaosantos.package-state-machine/Runtime/State.cs
@@ -1,26 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 namespace JoaoSant0s.StateMachine
 {
-    public class StateController
+    public abstract partial class State<T> where T: StateMachineController<T>
     {
-        protected StateMachineController machineController;
+        protected T machineController;
 
-        public StateController(StateMachineController controller)
+        public State(T controller)
         {
             this.machineController = controller;
         }
+
+        public abstract void OnBeging();
+
+        public abstract void OnUpdate();
+
+        public abstract void OnLateUpdate();
+
+        public abstract void OnFixedUpdate();
+
+        public abstract void OnFinish();
 
         /// <summary>
         /// Make the current state machine go to the next state
         /// </summary>
         /// <param name="nextState"> the next state object</param>
-        protected void ChangeState(IState nextState)
+        protected void ChangeState(State<T> nextState)
         {
             this.machineController.ChangeState(nextState);
         }
-
     }
 }
